669713b1d5b960912fa617093cf6ed46f6bf46c7
pgigoux <pgigoux@gemini.edu>
2019-10-17 18:06:24 -0300

    Added logging and exception trapping.

--- nuvuMon.py
+++ nuvuMon.py
@@ -5,6 +5,7 @@ import epics
 import os
 import time
 import re
+import logging
 from datetime import datetime, timedelta
 
 # Environment information for AOM and NGS2 IOCs
@@ -14,38 +15,70 @@ NGS2USER = 'root'
 NGS2PASS = 'ngs2@cERROp'
 NGS2CMD = '/opt/ao/bin/aocmd "tcp://localhost:45000" "STATUS"'
 os.environ['EPICS_CA_ADDR_LIST'] = NGS2IP + ' ' + AOMIP
+
+logging.basicConfig(filename="nuvu.log", level=logging.INFO, format='%(levelname)s:%(asctime)s: %(message)s')
+
 # Make connection to Nuvu Temperature record on AOM IOC
-ngs2nvtemp = epics.PV('aom:ngs2:tempNuvu')
+try:
+    ngs2nvtemp = epics.PV('aom:ngs2:tempNuvu')
+except Exception as e:
+    logging.exception(str(e))
+    exit(0)
+
 # Make ssh connection to NGS2 rtc
-ngs2 = paramiko.SSHClient()
-ngs2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ngs2.connect(NGS2IP, username=NGS2USER, password=NGS2PASS)
-time.sleep(0.2) # Sleep to give time to slow connection to return valid state
+try:
+    ngs2 = paramiko.SSHClient()
+    ngs2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ngs2.connect(NGS2IP, username=NGS2USER, password=NGS2PASS)
+except Exception as e:
+    logging.exception(str(e))
+    exit(0)
+
+# Sleep to give time to slow connection to return valid state
+time.sleep(0.2)
+
 # Loop forever to update temperature
-while(True):
-    startWhile = datetime.now() # Used to calculate total loop time
+while True:
+    startWhile = datetime.now()  # Used to calculate total loop time
     nuvuTemp = ''
     # Execute aocmd on NGS2 rtc
-    stdin, stdout, stderr = ngs2.exec_command(NGS2CMD)
+    try:
+        stdin, stdout, stderr = ngs2.exec_command(NGS2CMD)
+    except Exception as e:
+        logging.exception(str(e))
+        time.sleep(60)
+        continue
+
     for line in stdout:
         # Search for camera body temp on aocmd STATUS output
         tempsrch = re.search('body', line)
-        if not(tempsrch == None):
+        if tempsrch is not None:
             nuvuTemp = line.split('=')[1]
+
     # If there's no reading from NGS2, assume is down and publish nonsensical
     # temperature, else publish Nuvu Cam Temp
-    if nuvuTemp == '':
-        ngs2nvtemp.put(float(NaN))
-    else:
-        ngs2nvtemp.put(nuvuTemp)
+    try:
+        if nuvuTemp == '':
+            ngs2nvtemp.put(float('nan'))
+            logging.error('Null temperature')
+        else:
+            ngs2nvtemp.put(nuvuTemp)
+            logging.info('Temperature: {0}'.format(nuvuTemp))
+    except Exception as e:
+        logging.exception(str(e))
+        time.sleep(10)
+        continue
+
     # Calculate while loop exec time
     currTime = datetime.now()
     loopTime = (currTime - startWhile).total_seconds()
+    logging.info('Loop time: {0}'.format(loopTime))
     waitTime = 10 - loopTime
     if loopTime > 10:
-        print("Loop took too long: {0} [s]".format(loopTime))
+        logging.error('Loop took too long')
         continue
+
     # Wait until checking again
     time.sleep(waitTime)
-ngs2.close() # Close ssh connection
 
+ngs2.close()  # Close ssh connection

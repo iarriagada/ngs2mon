f13be4c92d289091cb53b05c31fc6fbec501803d
Ignacio Arriagada <iarriagada@gemini.edu>
2021-12-14 21:25:14 -0500

    Added keys for ssh connection to ngs2 machine

--- scripts/nuvuMon.py
+++ scripts/nuvuMon.py
@@ -12,7 +12,7 @@ from datetime import datetime, timedelta
 AOMIP = '172.17.65.100'
 NGS2IP = '172.17.65.31'
 NGS2USER = 'software'
-NGS2PASS = 'Aliquamliber0!'
+NGS2SSHKEY = '/home/software/.ssh/id_rsa'
 NGS2CMD = '/opt/ao/bin/aocmd "tcp://localhost:45000" "STATUS"'
 TEMP_HHL = 50 # Nuvu camera Temperature limit in Celsius
 
@@ -51,7 +51,7 @@ if __name__ == '__main__':
     try:
         ngs2 = paramiko.SSHClient()
         ngs2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ngs2.connect(NGS2IP, username=NGS2USER, password=NGS2PASS)
+        ngs2.connect(NGS2IP, username=NGS2USER, key_filename=NGS2SSHKEY)
     except Exception as e:
         nuvu_log.exception(str(e))
         exit(0)
